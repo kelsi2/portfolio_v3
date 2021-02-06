3517e7a6eaf81a5f46c052f7ac5193833caf633b
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-02-05 18:29:15 -0800

    Troubleshooting nodemailer for launch

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.listen(PORT, () => {
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS
